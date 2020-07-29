ee2e8fa506dfccc83f8267fd8bc9c30d2d4e0d5a
George Grainger <GraingerGY@cardiff.ac.uk>
2020-07-30 00:00:29 +0100

    Update main.js
    Add some convenience methods to the ball object:
    applyAccel - Is used to apply an acceleration to the ball
    updatePos - Is used to update the ball's position according to it's velocity
    draw - Draws the ball on the canvas

--- main.js
+++ main.js
@@ -8,11 +8,71 @@ const ball = {
 		y: 0
 	},
 	mass: 0.05,
-	radius: 15,
-	bounceMult: 0.5
+	radius: 1,
+	/** The velocity multiplier applied when the ball bounces */
+	bounceMult: 0.5,
+	/** The fillstyle applied to the ball when it is drawn. */
+	fillStyle: 'red',
+
+	//#region Convenience methods
+	/**
+	 * Apply a given acceleration to the ball over the given time period.
+	 * @param {Number} x The acceleration to apply on the X axis (m/s/s).
+	 * @param {Number} y The acceleration to apply on the Y axis (m/s/s).
+	 * @param {Number} timePeriod The time (in milliseconds) to apply this acceleration over (if 0 will be applied all at once). Defaults to 0.
+	 * @param {Number} numSamples The number of samples to take accross the time period (defaults to 100).
+	 */
+	applyAccel: function(x, y, timePeriod = 0, numSamples = 100) {
+		if (timePeriod == 0) {
+			this.velocity.x += x;
+			this.velocity.y += y;
+		} else {
+			const xPer = x / numSamples;
+			const yPer = y / numSamples;
+			var i = 0;
+			const intervalID = setInterval(() => {
+				this.velocity.x += xPer;
+				this.velocity.y += yPer;
+				i++;
+				if (i == numSamples) {
+					clearInterval(intervalID);
+				}
+			}, timePeriod / numSamples);
+		}
+	},
+
+	/**
+	 * Draw the ball on the given canvas context.
+	 * @param {Number} xOffset Number of pixels to offset the X axis by
+	 * @param {Number} yOffset Number of pixels to offset the Y axis by
+	 * @param {CanvasRenderingContext2D} ctx The drawing context to use.
+	 * @param {Number} zoom The zoom multiplier.
+	 */
+	draw: function(xOffset, yOffset, ctx, zoom = 1) {
+		let x = this.pos.x * zoom + xOffset;
+		let y = this.pos.y * zoom + yOffset;
+		ctx.moveTo(x, y + this.radius * zoom);
+		ctx.beginPath();
+		ctx.arc(x, y, this.radius * zoom, 0, Math.PI * 2);
+		ctx.fillStyle = this.fillStyle;
+		ctx.fill();
+	},
+
+	/**
+	 * Update the position of the ball depending on it's current velocity.
+	 * @param {Number} timePassed The amount of time to simulate (time passed since last update call), defaults to 1000.
+	 */
+	updatePos: function(timePassed = 1000) {
+		this.pos.x += this.velocity.x * (timePassed / 1000);
+		this.pos.y += this.velocity.y * (timePassed / 1000);
+	}
+
+	//#endregion
 };
 
 const fps = 60;
+
+//#region Canvas setup
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -29,17 +89,21 @@ function onResize() {
 window.addEventListener('resize', onResize);
 window.dispatchEvent(new Event('resize'));
 
-function fixedUpdate() {}
+//#endregion
 
+//#region Frame drawing
 function drawFrame() {
+	ball.updatePos(1000 / fps);
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	let x = ball.pos.x + drawOffset[0];
-	let y = ball.pos.y + drawOffset[1];
-	ctx.moveTo(x, y + ball.radius);
-	ctx.beginPath();
-	ctx.arc(x, y, ball.radius, 0, Math.PI * 2);
-	ctx.fillStyle = 'red';
-	ctx.fill();
+	ball.draw(drawOffset[0], drawOffset[1], ctx, 20);
 }
 
 setInterval(drawFrame, 1000 / fps);
+//#endregion
+
+//#region Ball gravity
+setInterval(() => {
+	ball.applyAccel(0, 9.81, 1000, fps);
+}, 1000);
+ball.applyAccel(0, 9.81, 1000, fps);
+//#endregion
